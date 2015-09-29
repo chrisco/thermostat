6a1ca7d2fb7870403bfb75e2d94a8b06cd212ed5
Gustaf Rudholm <gustafrudholm@Gustafs-MacBook-Pro.local>
2015-09-29 16:38:31 +0200

    add color formatting in webapp

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ var thermostat = new Thermostat();
 
 function updateDisplay() {
     $("#current_temp").html(thermostat.currentTemp);
+    $("#current_temp").css('color', thermostat.getColor());
 }
 
 $(document).ready(function () {
